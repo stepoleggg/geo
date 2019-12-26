f1eed3ab1c9655a7655d3768416210178dc187a9
stepoleggg <olegstefanoyv1@gmail.com>
2019-12-26 23:41:24 +0500

    added country blocks

--- answer.py
+++ answer.py
@@ -3,84 +3,108 @@ import random
 import json
 from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 
+def load_data():
+    with open('data/save.json') as json_file:
+        data = json.load(json_file)
+        return data
+
 def get_default_keys():
     keyboard = VkKeyboard(one_time=True)
     keyboard.add_button('Пропустить', color=VkKeyboardColor.DEFAULT)
     keyboard.add_button('Подсказка', color=VkKeyboardColor.POSITIVE)
+    keyboard.add_line()
+    keyboard.add_button("Список заучиваемых стран", color=VkKeyboardColor.DEFAULT)
+    keyboard.add_button("Добавить "+str(n)+" новых стран", color=VkKeyboardColor.POSITIVE)
+
+
     return keyboard
 
 def save_data():
     with open('data/save.json', 'w') as json_file:
         json.dump(user_data, json_file)
 
-def load_data():
-    with open('data/save.json') as json_file:
-        data = json.load(json_file)
-        return data
+def skip_country(user_id):
+    user_data[user_id]["country"] = random.choice(list(user_data[user_id]["set"]))
+    user_data[user_id]["i"] = 0
 
-df = pd.read_csv("data/data.csv")
-data = {}
-user_data = load_data()
-for line in df[[";;;;"]].to_numpy():
-    country = line[0].split(";")[0]
-    capital= line[0].split(";")[1]
-    side = line[0].split(";")[4]
-    data[country] = capital
+def init():
+    df = pd.read_csv("data/data.csv")
+    for line in df[["Страна;Столица;;;"]].to_numpy():
+        country = line[0].split(";")[0]
+        capital = line[0].split(";")[1]
+        side = line[0].split(";")[4]
+        data_capitals[country] = capital
+
+def get_set(user_id):
+    return "\n".join(user_data[user_id]["set"])
 
 def answer(mes, user_id):
     user_id = str(user_id)
     if not user_id in user_data.keys():
         user_data[user_id] = {}
-        user_data[user_id]["country"] = random.choice(list(data.keys()))
-        user_data[user_id]["i"] = 0
+        user_data[user_id]["set"] = []
+        for country in data_capitals:
+            if len(user_data[user_id]["set"]) < n:
+                user_data[user_id]["set"].append(country)
+        skip_country(user_id)
         save_data()
-        return "Страна: "+user_data[user_id]["country"], True, user_data[user_id]["country"], get_default_keys()
+        return "Стартовые страны: \n\n"+get_set(user_id)+"\n\nОтгадайте столицу страны: "+user_data[user_id]["country"], True, user_data[user_id]["country"], get_default_keys()
     i = user_data[user_id]["i"]
     country = user_data[user_id]["country"]
     inp = mes
 
-
     if inp == "пропустить":
-        user_data[user_id]["country"] = random.choice(list(data.keys()))
-        user_data[user_id]["i"] = 0
+        skip_country(user_id)
         next_country = user_data[user_id]["country"]
         save_data()
-        return "Ответ: "+data[country]+"\n\nСледующая страна: "+next_country, True, next_country, get_default_keys()
+        return "Ответ: "+data_capitals[country]+"\n\nСледующая страна: "+next_country, True, next_country, get_default_keys()
 
-    if inp.lower().replace("(","").replace(")","").replace("-","").replace(" ","") == data[country].lower().replace("(","").replace(")","").replace("-","").replace(" ",""):
-        user_data[user_id]["country"] = random.choice(list(data.keys()))
-        user_data[user_id]["i"] = 0
+    if inp.lower().replace("(","").replace(")","").replace("-","").replace(" ","") == data_capitals[country].lower().replace("(","").replace(")","").replace("-","").replace(" ",""):
+        skip_country(user_id)
         next_country = user_data[user_id]["country"]
         save_data()
         return "Правильно!\n\nСледующая страна: "+next_country, True, next_country, get_default_keys()
 
-    if i+1 < len(data[country]) and inp == "подсказка":
-        out = "Подсказка: "+data[country][:i+1]
+    if i+1 < len(data_capitals[country]) and inp == "подсказка":
+        out = "Подсказка: "+data_capitals[country][:i+1]
         i+=1
         user_data[user_id]["i"] = i
         save_data()
         return out, False, country, get_default_keys()
     
     if inp == "подсказка":
-        user_data[user_id]["country"] = random.choice(list(data.keys()))
-        user_data[user_id]["i"] = 0
+        skip_country(user_id)
         next_country = user_data[user_id]["country"]
         save_data()
-        return "Ответ: "+data[country]+"\n\nСледующая страна: "+next_country, True, next_country, get_default_keys()
+        return "Ответ: "+data_capitals[country]+"\n\nСледующая страна: "+next_country, True, next_country, get_default_keys()
+
+    if inp == "добавить "+str(n)+" новых стран":
+        current_len = len(user_data[user_id]["set"])
+        for country in list(data_capitals.keys())[current_len:current_len+n]:
+            user_data[user_id]["set"].append(country)
+        next_country = user_data[user_id]["country"]
+        save_data()
+        return "Страны добавлены. Заучиваемые страны: \n\n"+get_set(user_id)+"\n\nСледующая страна: "+next_country, True, next_country, get_default_keys()
+
+    if inp == "список заучиваемых стран":
+        next_country = user_data[user_id]["country"]
+        return "Заучиваемые страны: \n\n"+get_set(user_id)+"\n\nСледующая страна: "+next_country, True, next_country, get_default_keys()
     
-    if i+1 < len(data[country]):
-        out = "Неверно!\nПодсказка: "+data[country][:i+1]
+    if i+1 < len(data_capitals[country]):
+        out = "Неверно!\nПодсказка: "+data_capitals[country][:i+1]
         i+=1
         user_data[user_id]["i"] = i
         save_data()
         return out, False, country, get_default_keys()
 
-    
-
-    user_data[user_id]["country"] = random.choice(list(data.keys()))
-    user_data[user_id]["i"] = 0
+    skip_country(user_id)
     next_country = user_data[user_id]["country"]
     save_data()
-    return "Неверно!\nОтвет: "+data[country]+"\n\nСледующая страна: "+next_country, True, next_country, get_default_keys()
+    return "Неверно!\nОтвет: "+data_capitals[country]+"\n\nСледующая страна: "+next_country, True, next_country, get_default_keys()
 
-    
+
+n = 5
+data_capitals = {}
+data_sides = {}
+user_data = load_data()
+init()
